8c8a6ef3a09417eeb1a253809504661226835198
Divya Mamgai <7350522+divyamamgai@users.noreply.github.com>
2026-01-15 23:12:22 +0000

    Add more products

--- src/lib/components/ProductListing.svelte
+++ src/lib/components/ProductListing.svelte
@@ -4,8 +4,8 @@
 	const { product }: { product: Product } = $props();
 </script>
 
-<div class="grid h-128 p-8 grid place-items-start border-2 rounded-lg border-gray-300 hover:border-gray-700">
-	<img class="h-40 place-self-center" src={product.imageUrl} alt={product.name} />
+<div class="grid p-8 grid place-items-start border-2 rounded-lg border-gray-300 hover:border-gray-700">
+	<img class="h-40 place-self-center object-contain" src={product.imageUrl} alt={product.name} />
 	<a class="font-bold mt-4" href={product.url}>{product.name}</a>
 	<span class="mt-2">{product.price}</span>
 </div>
