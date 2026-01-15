0503c4736f76cfd2a589363f12d671fed307174f
Divya Mamgai <7350522+divyamamgai@users.noreply.github.com>
2026-01-15 23:26:39 +0000

    Add amazon affiliate tag

--- docs/_app/immutable/entry/start.CXv4-ch7.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CQa77b5w.js";export{o as load_css,r as start};
--- src/lib/components/ProductListing.svelte
+++ src/lib/components/ProductListing.svelte
@@ -4,8 +4,15 @@
 	const { product }: { product: Product } = $props();
 </script>
 
-<div class="grid p-8 grid place-items-start border-2 rounded-lg border-gray-300 hover:border-gray-700">
+<div
+	class="grid p-8 grid place-items-start border-2 rounded-lg border-gray-300 hover:border-gray-700"
+>
 	<img class="h-40 place-self-center object-contain" src={product.imageUrl} alt={product.name} />
-	<a class="font-bold mt-4" href={product.url}>{product.name}</a>
+	<a
+		class="font-bold mt-4"
+		target="_blank"
+		href="{product.url}?&linkCode=ll1&tag=pokestockwuphf-21&language=en_GB&ref_=as_li_ss_tl"
+		>{product.name}</a
+	>
 	<span class="mt-2">{product.price}</span>
 </div>
