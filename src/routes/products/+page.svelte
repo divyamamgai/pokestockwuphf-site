55d51d58298e131b1e909e33d24fd41b67d8514d
Divya Mamgai <7350522+divyamamgai@users.noreply.github.com>
2026-01-15 22:58:41 +0000

    Fix products page path

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -3,8 +3,8 @@
 	import ProductListing from '$lib/components/ProductListing.svelte';
 </script>
 
-<h1 class="m-10 text-5xl font-bold">Amazon Products</h1>
-<h2 class="ml-10 text-2xl font-bold">Available Invitations</h2>
+<h1 class="m-10 text-5xl font-bold">Products</h1>
+<h2 class="ml-10 text-2xl font-bold">Amazon Invitations</h2>
 <div class="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-10 m-10">
 	{#each productsAmazon as productAmazon}
 		<ProductListing product={productAmazon} />
